9cda702de764e0ff946a16a0b38503e1504059b8
Cruceru Nicusor <crucerunicusor04@gmail.com>
2020-06-09 18:18:13 +0300

    add a problem

---
+++ Concurs/pb2525-cioc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+ifstream f("cioc.in");
+ofstream g("cioc.out");
+
+int t[200002];
+
+int main(){
+    int n, c, k, kf; 
+    f >> c >> n >> k;
+    for(int i = 1; i <= n; i++){
+        f >> t[i];   
+    }
+
+    if(c == 1){
+        sort(t, t + n + 1);
+        kf = k;
+        for(int i = 1; (i <= k) || (t[i] == t[i+1]); i++){
+            if(t[i] == t[i+1]) kf++;    
+        }
+       
+        g << kf;
+    }
+}
